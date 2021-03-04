bbc9a52470b90d163de2c3e88ae89301e55f7fb9
Damian Duszyński <dd.duszynski@gmail.com>
2021-03-04 18:40:08 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener"
         >
-          Learn React - commit 1
+          Learn React - commit 2
         </a>
       </header>
     </div>
